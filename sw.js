9021a06017d79f74683edea605177309ac9773e3
selis-bang-aji <fajriselisbeam@gmail.com>
2026-07-28 08:55:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', function(event) {
-    // Kode pancingan agar Chrome mengizinkan instalasi aplikasi full screen
+    // Jalur aktivasi PWA Chrome
 });
